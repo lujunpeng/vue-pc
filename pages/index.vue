ebe4bdb61442bc3a305eab1f5fdf944dfe7d955c
lujunpeng <1002015928@qq.com>
2019-12-13 21:45:43 +0800

    123

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,20 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        vue-demos
+        lujunpeng
       </h1>
-      <h2 class="subtitle">
+      <!-- <h2 class="subtitle">
         Nuxt.js project
-      </h2>
+      </h2> -->
       <div class="links">
-        <a
+        <!-- <a
           href="https://nuxtjs.org/"
           target="_blank"
           class="button--green">Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="button--grey">GitHub</a> -->
       </div>
     </div>
   </section>
